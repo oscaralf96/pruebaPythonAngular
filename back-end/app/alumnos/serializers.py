0d576f9964d1eb0eee0b21400723e0ab2de4bd7a
Oscar Gonzalez <racsogonzalez30@gmail.com>
2025-02-09 23:36:55 -0600

    modulo alumnos y endpoints 'crear-alumno' y 'consultar-alumno/{idGrado}'

--- back-end/app/alumnos/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Alumno
-
-class AlumnoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Alumno
-        fields = '__all__'
